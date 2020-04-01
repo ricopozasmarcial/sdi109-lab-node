89cf1477ef7b166d136605f02a5c7e78d246b46a
marshall63991 <marshall6399@gmail.com>
2020-04-01 02:07:48 +0200

    SDI-NodeJS-9.6- Sistema de paginación

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -75,21 +75,37 @@ module.exports = function(app, swig,  gestorBD) {
     app.get("/tienda", function(req, res) {
         let criterio = {};
         if( req.query.busqueda != null ){
-            criterio = {"nombre" : {$regex : ".*"+req.query.busqueda+".*"} };
+            criterio = { "nombre" : req.query.busqueda };
         }
-        gestorBD.obtenerCanciones(criterio, function(canciones) {
+        let pg = parseInt(req.query.pg); // Es String !!!
+        if ( req.query.pg == null){ // Puede no venir el param
+            pg = 1;
+        }
+        gestorBD.obtenerCancionesPg(criterio, pg , function(canciones, total ) {
             if (canciones == null) {
                 res.send("Error al listar ");
             } else {
+                let ultimaPg = total/4;
+                if (total % 4 > 0 ){ // Sobran decimales
+                    ultimaPg = ultimaPg+1;
+                }
+                let paginas = []; // paginas mostrar
+                for(let i = pg-2 ; i <= pg+2 ; i++){
+                    if ( i > 0 && i <= ultimaPg){
+                        paginas.push(i);
+                    }
+                }
                 let respuesta = swig.renderFile('views/btienda.html',
                     {
                         canciones : canciones,
-
+                        paginas : paginas,
+                        actual : pg
                     });
                 res.send(respuesta);
             }
         });
     });
+
 
     app.get('/cancion/:id', function (req, res) {
         let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id) };
