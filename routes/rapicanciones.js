875c7fdc68cb3256c0cac274d3ead91c595e46ce
marshall63991 <marshall6399@gmail.com>
2020-04-08 02:39:13 +0200

    SDI-NodeJS-10.2- SW API REST Autenticación

--- routes/rapicanciones.js
+++ routes/rapicanciones.js
@@ -69,6 +69,8 @@ module.exports = function(app, gestorBD) {
             }
         });
 
+
+
     });
 
     app.put("/api/cancion/:id", function(req, res) {
@@ -98,4 +100,31 @@ module.exports = function(app, gestorBD) {
         });
     });
 
+    app.post("/api/autenticar/", function(req,res) {
+        let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+            .update(req.body.password).digest('hex');
+        let criterio = {
+            email : req.body.email,
+            password : seguro
+        }
+
+        gestorBD.obtenerUsuarios(criterio, function(usuarios) {
+            if(usuarios==null || usuarios.length == 0){
+                res.status(401);
+                res.json({autenticado:false})
+            }
+            else {
+                var token = app.get('jwt').sign({
+                    usuario: criterio.email,
+                    tiempo: Date.now()/1000
+                }, "secreto");
+                res.status(200);
+                res.json(
+                    {
+                        autenticado: true,
+                        token: token
+                    })
+            }
+        })
+    });
 }
